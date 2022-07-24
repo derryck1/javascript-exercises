d473569721922039585f6c28e4923b052bbb885c
derryck1 <derryckadu@gmail.com>
2022-07-23 21:12:50 -0400

    Will Complete the First Exercis on Javascript.
    This exercise was about  learning how to make repeating strings using
    for loops. initially I got stuck writing the script, as I couldn't
    figure how to write a for loop that would work with paramaters outside
    of the loop itself. But I realized I had to nest the loop inside of a
    if statement for it to do what I wanted it too. I also could not get the
    loop initially to reach the rep statement, as it was outside of the
    bounds of the the for statement, so it would repeatedly return an
    empty string. I realized what was happening and properly placed the
    rep expression inside of my for loop, and the addition stuck. Once I did
    that I realized that the tests weren't treating each test as a separate
    test, so I had to rewrite my expression so it would not do so.
    Next time i will commit more so I can see the differences between code
    when needed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let rep = "";
+    if (num >= 0 ) {
+   
+        for (i = 0; i  < num; i++) {
+        rep += string;
+    }
+ } else rep = 'ERROR';
+    return rep;
+    }
+    
+    //0 < 10  rep = "" + hey == hey
+    //1 < 10  rep = hey + hey = heyhey
+    //2 < 10  rep = heyhey + hey = heyheyhey 
+    //3 < 10  rep = heyheyhey + hey = heyheyheyhey
+    //4 < 10  rep = heyheyheyhey + hey = heyheyheyheyhey
+    //5 < 10  rep = heyheyheyheyhey + hey = heyheyheyheyheyhey
+    //6 < 10  rep = heyheyheyheyheyhey + hey = heyheyheyheyheyheyhey
+    //7 < 10  rep = heyheyheyheyheyheyhey + hey = heyheyheyheyheyheyheyhey
+    //8 < 10  rep = heyheyheyheyheyheyheyhey + hey = heyheyheyheyheyheyheyheyhey
+    //9 < 10  rep = heyheyheyheyheyheyheyheyhey + hey = heyheyheyheyheyheyheyheyheyhey
+    //10 < 10 rep =
+    //11 < 10 rep =
+    //12 < 10 rep = 
+    //13 < 10
 // Do not edit below this line
 module.exports = repeatString;
