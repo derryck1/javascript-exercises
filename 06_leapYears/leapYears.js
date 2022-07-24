2c161e79e9b541afc9cdc6af3b024bf36ac920df
derryck1 <derryckadu@gmail.com>
2022-07-24 19:08:22 -0400

    Will commit completed leap year exercises
    This one wasn't hard either. No loops so it was just about properly
    understanding the cases being expressed. Good reading practice and
    logic practice. False cases should usually go first.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let a;
 
+    if (((year % 100) == 0) && ((year % 400) != 0)) {
+        a = false;
+    } else if (((year % 4) == 0) && ((year % 400) == 0)) {
+        a = true;
+    } else if ((year % 4) == 0) {
+        a = true;
+    } else {
+        a = false;
+    }
+
+    return a;
 };
 
 // Do not edit below this line
