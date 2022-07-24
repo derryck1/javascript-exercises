3202a22d4121ba304f592e2b81f9773b75626328
derryck1 <derryckadu@gmail.com>
2022-07-23 22:01:32 -0400

    Add reverseString.js
    This will add my reverse string function.
    Took me a while even though writing the actual reverse script was easy
    I had messed up my for function using more variables than needed for the
    script but my head was in the right place. Global variables are usually
    not needed and actually hurt a lot of the time. Use local ones

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
 
-};
+
+const reverseString = function(string) {
+    let length = string.length;
+    let reversed = "";
+
+    for (i = length; i >= 0;  i--) { 
+        reversed += string.charAt(i);
+    }
+    return reversed;
+}
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//get a string
+// get the length of the string
+// take the last character of the string and add it to a new variable until
+// theres no items left
