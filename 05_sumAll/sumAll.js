d97ff2d8217a1b821ddf3f297a46e453ba8355a0
derryck1 <derryckadu@gmail.com>
2022-07-24 17:48:21 -0400

    Add Sum all to completed exercises
    This one wasn't hard once I got the logic around
    what was happening. What really helped was writing out
    what was supposed to happen both in plain english and
    in terms of the actual function using 1 - 4 as a test
    case. Might help to get a pen and paper when I cant logically
    figure out what something is supposed to do. was also good
    for practicing nesting loops within if statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let x = 0;
 
-};
+    if (((typeof num1) != 'number') || ((typeof num2) != 'number')) {
+        x = 'ERROR';
+    } else if ((num1 < 0) || (num2 < 0)) {
+        x = 'ERROR';
+    } else if (num1 > num2) {
+        let holder;
+        holder = num1;
+        num1 = num2;
+        num2 = holder;
+
+        for (let i = 1; i <= num2; i++) {
+            x += i;
+        }
+    } else {
+    for (let i = 1; i <= num2; i++) {
+        x += i;
+    }
+}
+    return x;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+//Read both integers (1,4)
+//Set X = to first integer
+//loop x = x + (x+1)
