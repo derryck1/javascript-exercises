f883e9891f805e7f6e722f545201ea0e5ed144f8
derryck1 <derryckadu@gmail.com>
2022-07-24 01:48:56 -0400

    Complete removeFromArrays script
    GOD DAMN. This one took me a while. Mainly because i wasn't reading
    the splice command properly and wasn't really understanding how it works
    Essentially though, splice can return a new array with your spliced out
    arrays, but the orignal array will still exist with that parts that
    havent been spliced out. Once i figured that out everything else was
    relatively easy. Reading is important. and the methods that you know
    will essentially be your survival in coding.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,52 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value, value2, value3, value4) {
+let i2;
 
-};
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            i2 = i
+            array.splice(i2, 1)
+        }
+    }
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === value2) {
+            i2 = i
+            array.splice(i2, 1)
+        }
+    }
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === value3) {
+            i2 = i
+            array.splice(i2, 1)
+        }
+    }
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] === value4) {
+            i2 = i
+            array.splice(i2, 1)
+        }
+    }
+
+    return array;
+}
+
+
+
+
+
+//[1, 2, 3, 4] , 3
+
+//0 array[0](1) == 3? no
+//1 array[1](2) == 3? no
+//2 array[2](3) == 3? no
+//3 array[3](4) == 3? no
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+//go through array
+// find value of array in array
+//slice out value
+//splice array together
